3c750ac918866be8ec62a32af0779d9cef6db18c
damA <lmollonb@iesch.org>
2022-03-01 14:54:32 +0100

    cambios

--- Tema 9/holaMundo/holaMundo/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ Tema 9/holaMundo/holaMundo/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace holaMundo {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/holaMundo;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/holaMundo;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
